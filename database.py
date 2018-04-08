9fffd6fa501378be85fb8a05ea46ca3cf758d10e
Misha Behersky <bmwant@gmail.com>
2018-04-08 13:24:02 +0300

    Add with_connection decorator :city_sunset:

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 from functools import wraps
 from contextlib import contextmanager
 
-import  attr
+import attr
 
 import config
 
@@ -30,24 +30,33 @@ def cursor():
 def with_connection(func):
     @wraps(func)
     def inner(*args, **kwargs):
-        return func(*args, **kwargs)
+        with cursor() as c:
+            return func(c, *args, **kwargs)
     return inner
 
 
 @with_connection
-def insert_new_meme():
-    with cursor() as c:
-        query = (
-            'INSERT INTO meme(active, alias, file_id, url) '
-            'VALUES(?, ?, ?, ?);'
-        )
-        values = (1, 'doge', 'AgADAgADBakxG8zCKEquNQl09cAjNe60qw4ABPRRFzZpUEqWgFkAAgI', 'https://example.com')
-        c.execute(query, values)
+def insert_new_meme(cursor):
+    query = (
+        'INSERT INTO meme(active, alias, file_id, url) '
+        'VALUES(?, ?, ?, ?);'
+    )
+    values = (1, 'doge', 'AgADAgADBakxG8zCKEquNQl09cAjNe60qw4ABPRRFzZpUEqWgFkAAgI', 'https://example.com')
+    cursor.execute(query, values)
 
 
-def get_meme_file_id():
-    pass
+@with_connection
+def get_meme_file_id(cursor, alias):
+    query = (
+        'SELECT * FROM meme WHERE alias = ?;'
+    )
+    cursor.execute(query, (alias,))
+    res = cursor.fetchone()
+    if res is None:
+        raise ValueError('No such meme %s' % alias)
+    return Meme(*res)
 
 
 if __name__ == '__main__':
-    insert_new_meme()
+    # insert_new_meme()
+    print(get_meme_file_id('doge'))
